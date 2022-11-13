034258673184f0cbb73c0ec7044f6a4c5f5132c3
Yusri Caloyloy <yusripawaki2@gmail.com>
2022-11-13 13:58:59 +0000

    feat: run client side

--- routes/api/crud.js
+++ routes/api/crud.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-require('dotenv').config({ path: '/env' });
+
 
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
